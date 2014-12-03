dc4556fe4bba9428f5d115f3e380cb6b3a547d39
Saikrishna Arcot <saiarcot895@gmail.com>
2014-12-03 12:51:00 -0500

    Finish creating the position class that will store each report for aircraft

--- position.cpp
+++ position.cpp
@@ -4,9 +4,16 @@
 class PositionData : public QSharedData {
 public:
     QDateTime reportingTime;
-    QGeoCoordinate coordinate;
-    quint32 speed;
-    quint32 track;
+    bool coordinatesSet = false;
+    qreal latitude;
+    qreal longitude;
+    bool altitudeSet = false;
+    qint32 altitude;
+    bool speedSet = false;
+    quint16 speed;
+    bool trackSet = false;
+    quint16 track;
+    bool squawkSet = false;
     quint16 squawk;
 };
 
@@ -16,6 +23,84 @@ Position::Position() : data(new PositionData)
 
 Position::Position(const Position &rhs) : data(rhs.data)
 {
+}
+
+QDateTime Position::getReportingTime() const {
+    return data->reportingTime;
+}
+
+bool Position::areCoordinatesSet() const {
+    return data->coordinatesSet;
+}
+
+qreal Position::getLatitude() const {
+    return data->coordinatesSet ? data->latitude : 0;
+}
+
+qreal Position::getLongitude() const {
+    return data->coordinatesSet ? data->longitude : 0;
+}
+
+bool Position::isAltitudeSet() const {
+    return data->altitudeSet;
+}
+
+qint32 Position::getAltitude() const {
+    return data->altitude;
+}
+
+bool Position::isSpeedSet() const {
+    return data->speedSet;
+}
+
+quint16 Position::getSpeed() const {
+    return data->speed;
+}
+
+bool Position::isTrackSet() const {
+    return data->trackSet;
+}
+
+quint16 Position::getTrack() const {
+    return data->track;
+}
+
+bool Position::isSquawkSet() const {
+    return data->squawkSet;
+}
+
+quint16 Position::getSquawk() const {
+    return data->squawk;
+}
+
+void Position::setReportingTime(QDateTime reportingTime) {
+    data->reportingTime = reportingTime;
+}
+
+void Position::setCoordinates(qreal latitude, qreal longitude) {
+    data->latitude = latitude;
+    data->longitude = longitude;
+    data->coordinatesSet = true;
+}
+
+void Position::setAltitude(qint32 altitude) {
+    data->altitude = altitude;
+    data->altitudeSet = true;
+}
+
+void Position::setSpeed(quint16 speed) {
+    data->speed = speed;
+    data->speedSet = true;
+}
+
+void Position::setTrack(quint16 track) {
+    data->track = track;
+    data->trackSet = true;
+}
+
+void Position::setSquawk(quint16 squawk) {
+    data->squawk = squawk;
+    data->squawkSet = true;
 }
 
 Position &Position::operator=(const Position &rhs)
--- position.h
+++ position.h
@@ -17,6 +17,26 @@ public:
     Position &operator=(const Position &);
     ~Position();
 
+    QDateTime getReportingTime() const;
+    bool areCoordinatesSet() const;
+    qreal getLatitude() const;
+    qreal getLongitude() const;
+    bool isAltitudeSet() const;
+    qint32 getAltitude() const;
+    bool isSpeedSet() const;
+    quint16 getSpeed() const;
+    bool isTrackSet() const;
+    quint16 getTrack() const;
+    bool isSquawkSet() const;
+    quint16 getSquawk() const;
+
+    void setReportingTime(QDateTime reportingTime);
+    void setCoordinates(qreal latitude, qreal longitude);
+    void setAltitude(qint32 altitude);
+    void setSpeed(quint16 speed);
+    void setTrack(quint16 track);
+    void setSquawk(quint16 squawk);
+
 private:
     QSharedDataPointer<PositionData> data;
 };
