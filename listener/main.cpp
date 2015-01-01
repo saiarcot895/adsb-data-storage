dc75c69d05082a30d376433c00ebe8787486885c
Saikrishna Arcot <saiarcot895@gmail.com>
2015-01-01 11:03:27 -0600

    Fix options parsing.

--- listener/main.cpp
+++ listener/main.cpp
@@ -10,19 +10,22 @@ int main(int argc, char *argv[])
     parser.setApplicationDescription("ADS-B data recorder");
     parser.addHelpOption();
 
-    QCommandLineOption hostOption(QStringList() << "h" << "host",
+    QCommandLineOption hostOption("host",
                                   QStringLiteral("Set the hostname of the ADS-B receiver."),
+                                  QStringLiteral("hostname"),
                                   QStringLiteral("localhost"));
     parser.addOption(hostOption);
 
     QCommandLineOption portOption(QStringList() << "p" << "port",
                                   QStringLiteral("Set the port to listen on."),
+                                  QStringLiteral("port"),
                                   QStringLiteral("30003"));
     parser.addOption(portOption);
 
     QCommandLineOption offsetOption("offset",
-                                  QStringLiteral("Set the offset from UTC (in hours) of the times being received from the receiver."),
-                                  QStringLiteral("0"));
+                                    QStringLiteral("Set the offset from UTC (in hours) of the times being received from the receiver."),
+                                    QStringLiteral("offset"),
+                                    QStringLiteral("0"));
     parser.addOption(offsetOption);
 
     // Process the actual command line arguments given by the user
